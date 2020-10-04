1310455f202cd2db74c3fe3bcec52bd377db5779
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-04 23:59:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,11 +11,11 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT MAX(age), name;"
+  "SELECT MAX(age) FROM bears, name;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT MIN(age), name;"
+  "SELECT MIN(age) FROM bears, name;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
